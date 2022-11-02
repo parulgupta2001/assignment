e5a5aed6743e487fa027196520e1c69646a6e991
Parul Gupta <94689580+parulgupta2001@users.noreply.github.com>
2022-11-03 00:13:06 +0530

    feat : main content component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
+import { MainContent } from "./components/mainContent/MainContent";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainContent />
     </div>
   );
 }
